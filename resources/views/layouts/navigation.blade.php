e004b8d1cf2c5bf4031e0b5134b251901dfa79ed
issacteoh <monozteoh@gmail.com>
2025-07-30 16:59:30 +0800

    weee, now need to setup for test.boaal.com ==

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -13,7 +13,7 @@
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                        {{ __('Dashboard') }}
+                        {{ __('Instructions') }}
                     </x-nav-link>
                     <x-nav-link :href="route('investors.index')" :active="request()->routeIs('investor*')">
                         {{ __('Investor') }}
--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
+Route::get('/instructions', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
@@ -18,6 +18,10 @@ Route::get("investors", [\App\Http\Controllers\InvestorController::class, 'index
 Route::get("funds", [FundController::class, 'index'])->name('funds.index');;
 Route::get("investments", [InvestmentController::class, 'index'])->name('investments.index');
 Route::get("graph", [GraphController::class, 'index'])->name('graph.index');
+Route::get('/exam', function () {
+    return view('exam.index');
+})->name('exam.index');
+
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
